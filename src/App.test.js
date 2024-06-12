7574ccb5b51dc3d777fc669c0e13fd6d3a48c455
sanchezcande <96777705+sanchezcande@users.noreply.github.com>
2024-06-12 09:50:54 -0300

    test update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 import { render, screen } from '@testing-library/react';
+import { act } from 'react'; // Nuevo
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {  
+    render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import Button from "../../components/Button/Button";
 const HomePage = React.forwardRef((props, ref) => (
   <Container ref={ref}>
     <Title>Bienvenido a la Página de Inicio</Title>
+    {/* borrar el style */}
     <Button style={{ marginBottom: "1000px" }} >Click Me</Button>
   </Container>
 ));
