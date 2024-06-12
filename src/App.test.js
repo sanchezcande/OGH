93ea8adbc9f564fe641ad166307334ad047d61a4
sanchezcande <96777705+sanchezcande@users.noreply.github.com>
2024-06-12 15:39:43 -0300

    node update and themeprovider in app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,16 @@ import { render, screen } from '@testing-library/react';
 import { act } from 'react';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 test('renders learn react link', () => {
   act(() => {
     render(
       <ErrorBoundary>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </ErrorBoundary>
     );
   });
