cf0a38d4204c448add6b69626d01ca5cf5564e16
sanchezcande <96777705+sanchezcande@users.noreply.github.com>
2024-06-12 10:15:34 -0300

    Add ErrorBoundary to handle errors in tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,12 +7,17 @@
 //   expect(linkElement).toBeInTheDocument();
 // });
 import { render, screen } from '@testing-library/react';
-import { act } from 'react'; // Nuevo
+import { act } from 'react';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
 
 test('renders learn react link', () => {
-  act(() => {  
-    render(<App />);
+  act(() => {
+    render(
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    );
   });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
