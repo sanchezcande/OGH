0289442054f9fcdcca3222a0c92c40548a6fcb3e
sanchezcande <actions@github.com>
2024-07-21 18:59:06 -0300

    font size

--- src/components/ContactForm/SuccessModal/SuccessModal.jsx
+++ src/components/ContactForm/SuccessModal/SuccessModal.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { ModalOverlay, ModalContent, CloseButton } from './SuccessModal.styles';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { ModalOverlay, ModalContent, CloseButton } from "./SuccessModal.styles";
 
-const SuccessModal = ({ message, onClose }) => (
-  <ModalOverlay>
-    <ModalContent>
-      <h2>Send!</h2>
-      <p>{message}</p>
-      <CloseButton onClick={onClose}>Close</CloseButton>
-    </ModalContent>
-  </ModalOverlay>
-);
+const SuccessModal = ({ message, onClose }) => {
+  const { t } = useTranslation();
+  return (
+    <ModalOverlay>
+      <ModalContent>
+        <h2>{t("modalSendTitle")}</h2>
+        <p>{message}</p>
+        <CloseButton onClick={onClose}>{t("modalCloseButtonText")}</CloseButton>
+      </ModalContent>
+    </ModalOverlay>
+  );
+};
 
 export default SuccessModal;
--- src/pages/ContactUs/ContactUs.styles.js
+++ src/pages/ContactUs/ContactUs.styles.js
@@ -10,6 +10,9 @@ export const Container = styled.div`
         font-size: 40px;
         font-weight: 700;
         margin-bottom: 10px;
+        @media (max-width: 768px) {
+            font-size: 30px;
+          }
     }
     h2 {
         text-align: center;
@@ -18,6 +21,9 @@ export const Container = styled.div`
         color: #828282;
         font-weight: 400;
         margin-bottom: 40px;
+        @media (max-width: 768px) {
+            font-size: 20px;
+          }
     }
 
     `;
