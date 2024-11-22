150c944184b8c9c695fa076bc1b762407bc625b3
Candelaria <sanchezgcandelaria@gmail.com>
2024-11-22 18:19:22 -0300

    draft 1

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,15 +1,31 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://opengatehub.com/</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
-  </url>
-</urlset>`;
+const DOMAIN = "https://opengatehub.com"; // Cambia por tu dominio.
 
-fs.writeFileSync(path.join(__dirname, 'public', 'sitemap.xml'), sitemapContent, 'utf8');
-console.log('sitemap.xml generado correctamente.');
+const paths = [
+  "/", 
+  "/about", 
+  "/contact", 
+];
+
+const generateSitemap = () => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${paths
+        .map((path) => {
+          return `
+            <url>
+              <loc>${DOMAIN}${path}</loc>
+              <changefreq>monthly</changefreq>
+              <priority>0.8</priority>
+            </url>
+          `;
+        })
+        .join("\n")}
+    </urlset>`;
+
+  fs.writeFileSync(path.join(__dirname, "public", "sitemap.xml"), sitemap, "utf8");
+};
+
+generateSitemap();
