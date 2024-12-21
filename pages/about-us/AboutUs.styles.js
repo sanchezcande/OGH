9d2df0307a98635fbbf6fe8b5ca5062485f0f300
OpenGateHub <hubopengate@gmail.com>
2024-12-21 17:03:42 -0300

    about us finished and buttons

--- pages/about-us/AboutUs.styles.js
+++ pages/about-us/AboutUs.styles.js
@@ -5,6 +5,8 @@ display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
+margin-left: 3rem;
+margin-right: 3rem;
 @media (max-width: 768px) {
   margin-top: 200px;
   
--- src/components/Box/BoxesContainerRow.styles.jsx
+++ src/components/Box/BoxesContainerRow.styles.jsx
@@ -1,25 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ContainerRow = styled.div`
-display: grid;
-grid-template-columns: repeat(4, 1fr); 
-gap: 40px;
-padding: 20px;
-margin: 0 auto;
-max-width: 1200px;
-justify-content: center;
-align-items: center;
-margin-top: 0px;
-padding-bottom: 50px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 15px;
+  padding: 20px;
+  max-width: 1200px;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 50px;
+  margin-left: 1rem;
+  margin-right: 1rem;
 
-@media (max-width: 1240px) {
-  grid-template-columns: repeat(2, 1fr);
-}
+  @media (max-width: 1240px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-@media (max-width: 768px) {
-  margin-top:50px;
-  grid-template-columns: 1fr; 
-  grid-gap: 20px;
-  padding: 0 20px;
-}
-    `;
+  @media (max-width: 768px) {
+    margin-top: 50px;
+    grid-template-columns: 1fr;
+    grid-gap: 20px;
+    padding: 0 20px;
+  }
+`;
